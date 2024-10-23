59cb4816ef9cde56095a7a0cc71427f024a6d490
GalenReed <galenreed@vt.edu>
2024-10-23 19:27:10 -0400

    a lil tricky with post

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+
+  const [quizQuestions, setQuizQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((data) => {
+        setQuizQuestions(data);
+      });
+  }, []);
+
+  function handleDelete(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then(() => {
+        setQuizQuestions(quizQuestions.filter((question) => question.id !== id));
+      });
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {quizQuestions.map((question) => (
+          <QuestionItem key={question.id} question={question} handleDelete={handleDelete}/>
+        ))}
+        </ul>
     </section>
   );
 }
